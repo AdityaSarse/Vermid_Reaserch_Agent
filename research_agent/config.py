9238c1f2f83bc47ae0d3bc41f0670f41724ee8ee
AdityaSarse <adityasarse362@gmail.com>
2026-06-25 15:58:34 +0530

    perf: disable HuggingFace online checks for faster startup

--- research_agent/config.py
+++ research_agent/config.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
+os.environ["HF_DATASETS_OFFLINE"] = "1"
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+
 ENTREZ_EMAIL = os.getenv("ENTREZ_EMAIL")
 NCBI_API_KEY = os.getenv("NCBI_API_KEY")
 TOP_K = int(os.getenv("TOP_K", 5))
